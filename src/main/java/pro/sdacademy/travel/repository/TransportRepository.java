af3df18af63627b153c10ca95fd7a96535d47275
Kęstutis Triponis <kestutis.triponis@gmail.com>
2021-07-04 13:30:27 +0300

    Add findTransportByDestinationName to transport repository

--- src/main/java/pro/sdacademy/travel/repository/TransportRepository.java
+++ src/main/java/pro/sdacademy/travel/repository/TransportRepository.java
@@ -3,11 +3,23 @@ package pro.sdacademy.travel.repository;
 import pro.sdacademy.travel.entity.Transport;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.UUID;
 
 public class TransportRepository extends AbstractCRUDRepository<UUID, Transport> {
 
     public TransportRepository(EntityManager entityManager) {
         super(entityManager, Transport.class);
+    }
+
+    public List<Transport> findTransportByDestinationName(String destinationName) {
+        return entityManager.createQuery("" +
+                        "SELECT t " +
+                        "FROM Transport t " +
+                        "JOIN t.destinations d " +
+                        "WHERE d.name = ?1",
+                Transport.class)
+                .setParameter(1, destinationName)
+                .getResultList();
     }
 }
--- src/main/java/pro/sdacademy/travel/test/TransportTestCase.java
+++ src/main/java/pro/sdacademy/travel/test/TransportTestCase.java
@@ -5,7 +5,7 @@ import pro.sdacademy.travel.entity.Transport;
 import pro.sdacademy.travel.repository.TransportRepository;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.stream.Collectors;
 
 import static pro.sdacademy.travel.entity.Transport.TransportType.BUS;
 
@@ -17,12 +17,16 @@ public class TransportTestCase extends BaseTestCase<Transport, TransportReposito
 
     @Override
     public Transport testCreate() {
+        return testCreate("LT", "UK");
+    }
+
+    private Transport testCreate(String... destinationName) {
         Transport transport = new Transport();
         transport.setNumber("ABC123");
         transport.setType(BUS);
-        transport.setDestinations(new HashSet<>(Arrays.asList(
-                new Destination("LT"),
-                new Destination("UK"))));
+        transport.setDestinations(Arrays.stream(destinationName)
+                .map(Destination::new)
+                .collect(Collectors.toSet()));
         repository.save(transport);
         return transport;
     }
@@ -31,5 +35,12 @@ public class TransportTestCase extends BaseTestCase<Transport, TransportReposito
     public void testUpdate(Transport transport) {
         transport.setNumber("AAA111");
         repository.save(transport);
+    }
+
+    public void extraTestFindTransportByDestinationName() {
+        testCreate("JP");
+        testCreate("JP");
+        testCreate("JP");
+        repository.findTransportByDestinationName("JP").forEach(System.out::println);
     }
 }
